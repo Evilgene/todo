dd5fbd3bd6690169153cab542ab5fedaf79bd922
Robert <mccannb@hotmail.com>
2022-10-09 23:03:31 +0100

    inital set up

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/todo", {
+    useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('connected to mongoose'))
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
+
+app.use('/', indexRouter)
+
+
+app.listen(3000)
+
